93dd5d4eb4d3af470dba61784c29e423fe8680c2
ping58972 <ping58972@gmail.com>
2019-05-04 01:05:29 -0500

    Updates

---
+++ precache-manifest.074e7df5c02d0cbd9081b63af74fea0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ba3d316222ac6fad7edb639cf381c8",
+    "url": "/react-practrice-FormEventHandle/index.html"
+  },
+  {
+    "revision": "c72ed10fea2fceac260d",
+    "url": "/react-practrice-FormEventHandle/static/css/main.68fb396b.chunk.css"
+  },
+  {
+    "revision": "772e8e6f33ce5be03245",
+    "url": "/react-practrice-FormEventHandle/static/js/2.65b9c135.chunk.js"
+  },
+  {
+    "revision": "c72ed10fea2fceac260d",
+    "url": "/react-practrice-FormEventHandle/static/js/main.8756d7d7.chunk.js"
+  },
+  {
+    "revision": "4720c5c9cb8fa3852de5",
+    "url": "/react-practrice-FormEventHandle/static/js/runtime~main.c6cb3db6.js"
+  }
+]);
