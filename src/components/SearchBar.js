f71e204b128d95fd8e90e51dfc52a1f13d3feecd
ping58972 <ping58972@gmail.com>
2019-05-04 01:14:11 -0500

    deploy to github-page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,6 +15,7 @@ class SearchBar extends React.Component {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
         <div className="field">
+        <h1 style={{color:'#bbb'}}>You can use any keyword to search out the beautiful pictures!</h1>
         <label htmlFor="">Image Search</label>
         <input type="text" value= {this.state.term} onChange = {e=> this.setState({term:e.target.value})}/>
         </div>
